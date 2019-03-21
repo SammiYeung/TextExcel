0cf48106ef93feb3c45e3e4c05ae1696b4fd5ab7
samsamsenpai <samsamsenpai@10.208.40.193>
2019-03-21 10:16:43 -0700

    Checkpoint 2 works!

---
+++ TextExcel/src/textExcel/RealCell.java
@@ -0,0 +1,9 @@
+//@author Sammi Yeung
+//@version March 21, 2019
+//This code is the superclass of ValueCell, PercentCell, and FormulaCell
+
+package textExcel;
+
+public class RealCell {
+
+}
--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -1,6 +1,6 @@
 //@author Sammi Yeung
 //@version March 12, 2019
-//This code should store String 
+//This code should store Strings
 
 package textExcel;
 
@@ -8,24 +8,24 @@ public class TextCell implements Cell {
 	private String str;	
 	
 	public TextCell(String command) { 
-		this.str = command; //string value of cell
+		this.str = command.substring(1,command.length()-1); //string value of cell
+			for (int i = 0; i <= 10; i++) { //adding 10 spaces
+				this.str += " ";
+			}
 	}
 	
 	public String abbreviatedCellText() { //has to return 10 chars
-		this.str = this.str.substring(1,str.length()-1);
-		this.str += "          ";
-		return this.str.substring(0,10);
+		return str.substring(0,10);
 		
 		
 		/* String alterStr = str;
-		 * for (int i = 0; i <= 10; i++) { //adding 10 spaces
-			alterStr += " ";
+		 * 
 		}
 		return alterStr.substring(0,10); //truncating to the first 10 chars */
 	}		
 
 	public String fullCellText() { //returns the original String with quotes
-		return "\""+ this.str.substring(0, str.length() - 10) + "\"" ;
+		return "\""+ this.str.substring(0, str.length() - 11) + "\"" ;
 	}
 
 }
