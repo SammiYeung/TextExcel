83564b57b20ca551b7ecc89b0669b263e2ed6e78
samsamsenpai <samsamsenpai@10.208.40.193>
2019-03-25 18:58:03 -0700

    Checkpoint 4 is coded, now troubleshooting and figuring out the error

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -1,9 +1,7 @@
 //@author Sammi Yeung
 //@version March 21, 2019
 //This code stores formula cells
-
 package textExcel;
-
 public class FormulaCell extends RealCell{
 	private String str;	
 	
@@ -11,11 +9,35 @@ public class FormulaCell extends RealCell{
 		super(command);
 	}
 	
-	public double getDoubleValue() {
-		return 10.0;
+	public double getDoubleValue(String str) {
+		String noParentheses = str.substring(2, str.length() - 1); //get rid of parentheses, first/last space
+		String[] splitFormula = noParentheses.split(" ");
+		double answer = Integer.parseInt(splitFormula[0]);
+		for (int i = 2; i <= splitFormula.length;) { //looking at the numbers only
+			for (int j = 1; j <= (splitFormula.length) - 1; j+=2) { //looking at operators only
+				if(splitFormula[j].equals("+")) { 
+					answer += Integer.parseInt(splitFormula[i]);
+					i+=2;
+				}
+				else if(splitFormula[j].equals("-")) {
+					answer -= Integer.parseInt(splitFormula[i]);
+					i+=2;
+				 }
+				else if(splitFormula[j].equals("*")) {
+					answer *= Integer.parseInt(splitFormula[i]);
+					i+=2;
+				}
+				else if(splitFormula[j].equals("/")) {
+					answer /= answer += Integer.parseInt(splitFormula[i]);
+					i+=2;
+				}
+			}
+		}
+		return answer;
 	}
 
 	public String abbreviatedCellText() { //has to print at least 10 chars and a % character at the end
+		getDoubleValue(str);
 		return str.substring(0,10);
 	}		
 
